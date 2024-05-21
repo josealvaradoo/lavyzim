cb08335b1fa10e53c4f3fd7f55b9b0c90b926143
Jose Alvarado <jose_alvarado17@hotmail.com>
2024-05-21 00:12:01 -0400

    adding feature to delete without cut off

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,3 +10,6 @@ map("n", "<Right>", '<cmd>echo "Arrow disabled, use l instead"<cr>', { desc = "M
 
 map("v", "J", ":m+1<cr>", { desc = "Move line to down" })
 map("v", "K", ":m-2<cr>", { desc = "Move line to up" })
+
+map("n", "dd", '"_dd<cr>', { desc = "Delete without cut off" })
+map("v", "d", '"_d<cr>', { desc = "Delete without cut off" })
