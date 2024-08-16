d511184b6748def9fa41775b3bd27f511a1a2f15
Jose Alvarado <jose_alvarado17@hotmail.com>
2024-08-16 02:57:50 -0400

    add custom keymaps using which-key.nvim

---
+++ lua/plugins/whichkey.lua
@@ -0,0 +1,24 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+  end,
+  -- your configuration comes here
+  -- or leave it empty to use the default settings
+  -- refer to the configuration section below
+  keys = {
+    {
+      "<leader>bc",
+      function()
+        -- :%bd - Deletes all buffers
+        -- |e# - Reopens the last buffer
+        -- |bd# - Deletes the alternate buffer, which is now the last buffer
+        -- '" Keep my current cursor position
+        vim.cmd(":%bd|e#|bd#|'\"")
+      end,
+      desc = "Close all buffers but current",
+    },
+  },
+}
