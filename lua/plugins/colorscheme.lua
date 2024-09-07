d81caff4ef0377373f727f5c86a32555d7535050
Jose Alvarado <jose_alvarado17@hotmail.com>
2024-09-07 17:00:22 -0400

    add chocolatier colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,11 @@
 return {
   { "catppuccin/nvim", name = "catppuccin", lazy = false, opts = { flavour = "macchiato" } },
   { "rebelot/kanagawa.nvim", name = "kanagawa", lazy = true, opts = { flavour = "dragon" } },
+  { "qaptoR-nvim/chocolatier.nvim", priority = 1000, config = true },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "chocolatier",
     },
   },
 }
