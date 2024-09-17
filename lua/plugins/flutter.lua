9eb9a2d4f3adf121f478ed715ab2c3ce86c81081
Jose Alvarado <jose_alvarado17@hotmail.com>
2024-09-17 02:12:43 -0400

    add plugin for highlight code chunks

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -2,10 +2,13 @@ return {
   {
     "akinsho/flutter-tools.nvim",
     lazy = false,
+    event = "VeryLazy",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "stevearc/dressing.nvim", -- optional for vim.ui.select
     },
-    config = true,
+    config = function()
+      require("flutter-tools").setup({}) -- default config
+    end,
   },
 }
---
+++ lua/plugins/hlchunk.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "shellRaining/hlchunk.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("hlchunk").setup({
+        chunk = {
+          enable = true,
+          priority = 15,
+          style = {
+            { fg = "#B3FFDD" }, -- #806d9c
+            { fg = "#c21f30" }, -- #c21f30
+          },
+          use_treesitter = true,
+          chars = {
+            left_arrow = "─",
+            horizontal_line = "─",
+            vertical_line = "│",
+            left_top = "╭",
+            left_bottom = "╰",
+            right_arrow = ">",
+          },
+          textobject = "",
+          max_file_size = 1024 * 1024,
+          error_sign = true,
+          duration = 200,
+          delay = 300,
+        },
+      })
+    end,
+  },
+}
