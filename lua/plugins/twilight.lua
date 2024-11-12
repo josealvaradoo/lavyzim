762097aa952a39001a3af07d1ff063d8a301bf2d
Jose Alvarado <jose_alvarado17@hotmail.com>
2024-11-12 10:57:09 -0400

    add twilight for focus (zen mode)

--- lua/plugins/twilight.lua
+++ lua/plugins/twilight.lua
@@ -5,4 +5,10 @@ return {
   -- URL: https://github.com/folke/twilight.nvim
   -- Description: A Neovim plugin for dimming inactive portions of the code you're editing.
   "folke/twilight.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
 }
