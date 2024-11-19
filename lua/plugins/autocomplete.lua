ca2fa390514a61931cd5fde78d2da1cf9eb18989
Jose Alvarado <jose_alvarado17@hotmail.com>
2024-11-19 16:52:47 -0400

    disable supermaven to be replaced by the Lazyvim one

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,54 +1,55 @@
+return {}
 -- Config autocomplete and code suggestions like IA Copilot
-return {
-  -- Supermaven is a fastest alternative to Github Copilot
-  {
-    "supermaven-inc/supermaven-nvim",
-    lazy = true,
-    event = "VeryLazy",
-    config = function()
-      require("supermaven-nvim").setup({})
-    end,
-  },
-  -- cmp is the plugin which manage the code suggestions
-  -- ane requries some sorces
-  {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "supermaven-inc/supermaven-nvim",
-    },
-    opts = function(_, opts)
-      local cmp = require("cmp")
-      local defaults = require("cmp.config.default")()
-      local auto_select = true
-
-      opts.mapping = cmp.mapping.preset.insert({
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-        ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<Tab>"] = LazyVim.cmp.confirm({ select = auto_select }),
-        ["<C-y>"] = LazyVim.cmp.confirm({ select = true }),
-        ["<S-CR>"] = LazyVim.cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        ["<C-CR>"] = function(fallback)
-          cmp.abort()
-          fallback()
-        end,
-      })
-      opts.sources = cmp.config.sources({
-        { name = "supermaven" },
-        { name = "nvim_lsp" },
-        { name = "buffer" },
-        { name = "path" },
-      })
-      opts.sorting = defaults.sorting
-
-      return opts
-    end,
-    main = "lazyvim.util.cmp",
-  },
-}
+-- return {
+--   -- Supermaven is a fastest alternative to Github Copilot
+--   {
+--     "supermaven-inc/supermaven-nvim",
+--     lazy = true,
+--     event = "VeryLazy",
+--     config = function()
+--       require("supermaven-nvim").setup({})
+--     end,
+--   },
+--   -- cmp is the plugin which manage the code suggestions
+--   -- ane requries some sorces
+--   {
+--     "hrsh7th/nvim-cmp",
+--     event = "InsertEnter",
+--     dependencies = {
+--       "hrsh7th/cmp-nvim-lsp",
+--       "hrsh7th/cmp-buffer",
+--       "hrsh7th/cmp-path",
+--       "supermaven-inc/supermaven-nvim",
+--     },
+--     opts = function(_, opts)
+--       local cmp = require("cmp")
+--       local defaults = require("cmp.config.default")()
+--       local auto_select = true
+--
+--       opts.mapping = cmp.mapping.preset.insert({
+--         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+--         ["<C-f>"] = cmp.mapping.scroll_docs(4),
+--         ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+--         ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+--         ["<C-Space>"] = cmp.mapping.complete(),
+--         ["<Tab>"] = LazyVim.cmp.confirm({ select = auto_select }),
+--         ["<C-y>"] = LazyVim.cmp.confirm({ select = true }),
+--         ["<S-CR>"] = LazyVim.cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+--         ["<C-CR>"] = function(fallback)
+--           cmp.abort()
+--           fallback()
+--         end,
+--       })
+--       opts.sources = cmp.config.sources({
+--         { name = "supermaven" },
+--         { name = "nvim_lsp" },
+--         { name = "buffer" },
+--         { name = "path" },
+--       })
+--       opts.sorting = defaults.sorting
+--
+--       return opts
+--     end,
+--     main = "lazyvim.util.cmp",
+--   },
+-- }
