28508d77558578a3432d00935b48384cce90a0c6
Jose Alvarado <jose_alvarado17@hotmail.com>
2025-01-11 18:28:44 -0400

    config oil

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -7,8 +7,9 @@ return {
     ---@module 'oil'
     ---@type oil.SetupOpts
     opts = {},
+    enabled = true,
     -- Optional dependencies
     -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+    dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
   },
 }
