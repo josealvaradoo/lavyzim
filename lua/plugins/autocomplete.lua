88b0a85108301013cf86868378a67b8045a189aa
Jose Alvarado <jose_alvarado17@hotmail.com>
2025-01-13 00:31:47 -0400

    set supermaven-nvim as autocompleter

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -8,7 +8,13 @@ return {
     lazy = true,
     event = "VeryLazy",
     config = function()
-      require("supermaven-nvim").setup({})
+      require("supermaven-nvim").setup({
+        keymaps = {
+          accept_suggestion = "<Tab>",
+          clear_suggestion = "<C-]>",
+          accept_word = "<C-j>",
+        },
+      })
     end,
   },
   {
