702bcbadb46bb9de094e911a3c512a06c2d79d8c
Jose Alvarado <jose_alvarado17@hotmail.com>
2025-02-09 20:33:19 -0400

    enable gemini as ia provider for avante

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -4,21 +4,25 @@ return {
   "yetone/avante.nvim",
   event = "VeryLazy",
   lazy = false,
-  version = false, -- set this if you want to always pull the latest change
+  version = false,
   opts = {
     ---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
-    provider = "claude", -- Recommend using Claude
-    auto_suggestions_provider = "gemini", -- Recommend using Copilot
+    provider = "gemini", -- Recommend using Claude
+    auto_suggestions_provider = "copilot", -- Recommend using Copilot
     copilot = {
       temperature = 0,
       model = "gpt-4o-2024-08-06",
     },
+    claude = {
+      temperature = 0,
+      model = "claude-3-5-sonnet-20241022",
+    },
     gemini = {
       temperature = 0,
-      model = "gemini-2.0-flash-exp",
+      model = "gemini-2.0-flash-thinking-exp-01-21",
     },
     behaviour = {
-      auto_suggestions = false, -- Experimental stage
+      auto_suggestions = true, -- Experimental stage
       auto_set_highlight_group = true,
       auto_set_keymaps = true,
       auto_apply_diff_after_generation = false,
@@ -118,7 +122,7 @@ return {
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    -- "zbirenbaum/copilot.lua", -- for providers='copilot'
+    "zbirenbaum/copilot.lua", -- for providers='copilot'
     "supermaven-inc/supermaven-nvim",
     {
       -- Make sure to set this up properly if you have lazy=true
