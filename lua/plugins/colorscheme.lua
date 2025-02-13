b8daebf1540a4f24523b760b4440702a4fc3ae00
Jose Alvarado <jose_alvarado17@hotmail.com>
2025-02-13 00:21:24 -0400

    update lazyvim

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,6 +6,7 @@ return {
     -- Plugin for the Catppuccin color scheme
     "catppuccin/nvim",
     name = "catppuccin",
+    enabled = false,
     lazy = false, -- Load this plugin immediately
     opts = {
       transparent_background = true, -- Enable transparent background
@@ -28,6 +29,7 @@ return {
     -- Plugin for the Kanagawa color scheme
     "rebelot/kanagawa.nvim",
     name = "kanagawa",
+    enabled = false,
     lazy = true,
     priority = 1000, -- Load this plugin after all other plugins
     opts = {
@@ -69,12 +71,9 @@ return {
       require("everforest").setup({
         background = "hard", -- Set the background to 'hard'
         italis = true, -- Enable italics
-        transparent_background_level = 1, -- Uncomment to enable transparency
+        -- transparent_background_level = 1, -- Uncomment to enable transparency
         diagnostic_text_highlight = true, -- Enable diagnostic text highlight
         diagnostic_virtual_text = "coloured", -- Set diagnostic virtual text to colored
-        colours_override = function(palette)
-          palette.bg0 = "#1A1A22" -- Override background color
-        end,
       })
     end,
   },
@@ -87,107 +86,14 @@ return {
         dark_variant = "moon", -- main, moon, or dawn
         dim_inactive_windows = false,
         extend_background_behind_borders = true,
-        groups = {
-          border = "muted",
-          link = "iris",
-          panel = "surface",
-
-          error = "love",
-          hint = "iris",
-          info = "foam",
-          note = "pine",
-          todo = "rose",
-          warn = "gold",
-
-          git_add = "foam",
-          git_change = "rose",
-          git_delete = "love",
-          git_dirty = "rose",
-          git_ignore = "muted",
-          git_merge = "iris",
-          git_rename = "pine",
-          git_stage = "iris",
-          git_text = "rose",
-          git_untracked = "subtle",
-
-          h1 = "iris",
-          h2 = "foam",
-          h3 = "rose",
-          h4 = "gold",
-          h5 = "pine",
-          h6 = "foam",
-        },
-        palette = {
-          main = {
-            _nc = "#16141f",
-            base = "#191724",
-            surface = "#1f1d2e",
-            overlay = "#26233a",
-            muted = "#6e6a86",
-            subtle = "#908caa",
-            text = "#e0def4",
-            love = "#eb6f92",
-            gold = "#f6c177",
-            rose = "#ebbcba",
-            pine = "#31748f",
-            foam = "#9ccfd8",
-            iris = "#c4a7e7",
-            leaf = "#95b1ac",
-            highlight_low = "#21202e",
-            highlight_med = "#403d52",
-            highlight_high = "#524f67",
-            none = "NONE",
-          },
-          moon = {
-            _nc = "#1f1d30",
-            base = "#232136",
-            surface = "#2a273f",
-            overlay = "#393552",
-            muted = "#6e6a86",
-            subtle = "#908caa",
-            text = "#e0def4",
-            love = "#eb6f92",
-            gold = "#f6c177",
-            rose = "#ea9a97",
-            pine = "#3e8fb0",
-            foam = "#9ccfd8",
-            iris = "#c4a7e7",
-            leaf = "#95b1ac",
-            highlight_low = "#2a283e",
-            highlight_med = "#44415a",
-            highlight_high = "#56526e",
-            FzfLuaBorder = "#56526e",
-            none = "NONE",
-          },
-          dawn = {
-            _nc = "#f8f0e7",
-            base = "#faf4ed",
-            surface = "#fffaf3",
-            overlay = "#f2e9e1",
-            muted = "#9893a5",
-            subtle = "#797593",
-            text = "#575279",
-            love = "#b4637a",
-            gold = "#ea9d34",
-            rose = "#d7827e",
-            pine = "#286983",
-            foam = "#56949f",
-            iris = "#907aa9",
-            leaf = "#6d8f89",
-            highlight_low = "#f4ede8",
-            highlight_med = "#dfdad9",
-            highlight_high = "#cecacd",
-            none = "NONE",
-          },
-        },
       })
     end,
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "rose-pine",
-      transparent = true,
+      colorscheme = "everforest",
+      transparent = false,
     },
   },
 }
