c0f2a45becb686655708d397c4e9bfc0f16dc8b1
Jose Alvarado <jose_alvarado17@hotmail.com>
2025-02-15 12:41:11 -0400

    update flutter config

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -2,60 +2,27 @@
 return {
   {
     "akinsho/flutter-tools.nvim",
-    lazy = true,
-    event = "VeryLazy",
+    lazy = false,
     dependencies = {
       "nvim-lua/plenary.nvim",
       "stevearc/dressing.nvim",
     },
     config = function()
-      require("flutter-tools").setup({
-        debugger = {
-          -- make these two params true to enable debug mode
-          enabled = false,
-          run_via_dap = false,
+      local map = LazyVim.safe_keymap_set
+      require("flutter-tools").setup({})
 
-          register_configurations = function(_)
-            require("dap").adapters.dart = {
-              type = "executable",
-              command = vim.fn.stdpath("data") .. "/mason/bin/dart-debug-adapter",
-              args = { "flutter" },
-            }
-
-            require("dap").configurations.dart = {
-              {
-                type = "dart",
-                request = "launch",
-                name = "Launch flutter",
-                dartSdkPath = "home/flutter/bin/cache/dart-sdk/",
-                flutterSdkPath = "home/flutter",
-                program = "${workspaceFolder}/lib/main.dart",
-                cwd = "${workspaceFolder}",
-              },
-            }
-          end,
-        },
-        dev_log = {
-          -- toggle it when you run without DAP
-          enabled = false,
-          open_cmd = "tabedit",
-        },
-      })
-      -- [[ Configure Flutter tools]]
-      -- vim.keymap.set(
-      --   "n",
-      --   "<leader>r",
-      --   require("telescope").extensions.flutter.commands,
-      --   { desc = "Open command Flutter" }
-      -- )
-      vim.keymap.set("n", "<leader>br", function()
-        vim.cmd("20new")
-        vim.cmd("te fvm flutter packages pub run build_runner build --delete-conflicting-outputs")
-        vim.cmd("2sleep | normal G")
-      end)
+      map("n", "<leader>r", "", { desc = "Flutter" })
+      map("n", "<leader>rr", ":FlutterRun<cr>", { desc = "Flutter Run" })
+      map("n", "<leader>rd", ":FlutterDebug<cr>", { desc = "Flutter Debug" })
+      map("n", "<leader>rp", ":FlutterHotReload<cr>", { desc = "Flutter Hot Reload" })
+      map("n", "<leader>rf", ":FlutterFlutterClean<cr>", { desc = "Flutter Flutter Clean" })
+      map("n", "<leader>rc", ":FlutterCreateProject<cr>", { desc = "Flutter Create Project" })
+      map("n", "<leader>ri", ":FlutterInspect<cr>", { desc = "Flutter Inspect" })
+      map("n", "<leader>rb", ":FlutterBuild<cr>", { desc = "Flutter Build" })
+      map("n", "<leader>rh", ":FlutterHotReloadApp<cr>", { desc = "Flutter Hot Reload App" })
+      map("n", "<leader>re", ":FlutterEmulators<cr>", { desc = "Flutter Emulators" })
     end,
   },
-  -- for dart syntax hightling
   {
     "dart-lang/dart-vim-plugin",
   },
