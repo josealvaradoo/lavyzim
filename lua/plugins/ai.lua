302fad92b0f859f5b3bbe87a452be5ecfa81875e
Jose Alvarado <jose_alvarado17@hotmail.com>
2025-05-24 05:39:54 -0400

    config copilot chat manually
    fix

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,5 +1,38 @@
 return {
   {
+    -- Plugin: CopilotChat.nvim
+    -- Repository: https://github.com/CopilotC-Nvim/CopilotChat.nvim
+    -- Description: A IA assistant by Github Copilot.
+    "CopilotC-Nvim/CopilotChat.nvim",
+    cmd = "CopilotChat",
+    opts = function()
+      local user = vim.env.USER or "User"
+      user = user:sub(1, 1):upper() .. user:sub(2)
+      return {
+        auto_insert_mode = false,
+        question_header = "  " .. user .. " ",
+        answer_header = "  Copilot",
+        model = "gemini-2.5-pro",
+        window = {
+          width = 0.4,
+        },
+      }
+    end,
+    config = function(_, opts)
+      local chat = require("CopilotChat")
+
+      vim.api.nvim_create_autocmd("BufEnter", {
+        pattern = "copilot-chat",
+        callback = function()
+          vim.opt_local.relativenumber = false
+          vim.opt_local.number = false
+        end,
+      })
+
+      chat.setup(opts)
+    end,
+  },
+  {
     -- Plugin: Code Companion
     -- Repository: https://github.com/olimorris/codecompanion.nvim
     -- Description: Code Companion is a plugin that provides a coding powered by IA feature.
