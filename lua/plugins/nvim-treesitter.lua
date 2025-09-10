f3b10d45ff635709eb492eeb8bb8a0eae733cd5c
Jose Alvarado <jose_alvarado17@hotmail.com>
2025-09-10 12:49:42 -0400

    add nvim-treesitter supported languages

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -5,4 +5,19 @@ return {
   build = ":TSUpdate",
   event = { "VeryLazy" },
   lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
+  opts = {
+    ensure_installed = {
+      "javascript",
+      "typescript",
+      "html",
+      "css",
+      "scss",
+      "go",
+      "php",
+      "cpp",
+      "tsx",
+      "vue",
+      "svelte",
+    },
+  },
 }
