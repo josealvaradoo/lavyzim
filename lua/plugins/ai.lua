24c689df6e99daf1c20dc053a7e7c3e6264fb05c
Jose Alvarado <jose_alvarado17@hotmail.com>
2026-03-03 20:23:23 -0400

    replace opencode.nvim plugin

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -179,51 +179,78 @@ return {
     end,
   },
   {
-    "NickvanDyke/opencode.nvim",
-    dependencies = {
-      ---@module 'snacks'
-      { "folke/snacks.nvim" },
-    },
+    "sudo-tee/opencode.nvim",
     config = function()
-      local map = LazyVim.safe_keymap_set
-
-      local select = function()
-        require("opencode").select()
-      end
-
-      local add = function()
-        require("opencode").prompt("@this")
-      end
-
-      local toggle = function()
-        require("opencode").toggle()
-      end
-
-      local ask = function()
-        require("opencode").ask("@this: ", { submit = true })
-      end
-
-      ---@type opencode.Opts
-      vim.g.opencode_opts = {
-        -- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition".
-      }
-
-      -- Required for `opts.events.reload`.
-      vim.o.autoread = true
-
-      map("n", "<leader>ao", "", { desc = "opencode" })
-      map("n", "<leader>aot", function()
-        toggle()
-      end, { desc = "Toggle" })
-      map("n", "<leader>aoa", function()
-        ask()
-      end, { desc = "Ask this" })
-      map({ "n", "v" }, "<leader>aos", function()
-        select()
-      end, { desc = "Execute opencode action…" })
-      map({ "n", "x" }, "<leader>ao>", function()
-        add()
-      end, { desc = "Add to opencode" })
+      require("opencode").setup({})
     end,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          anti_conceal = { enabled = false },
+          file_types = { "markdown", "opencode_output" },
+        },
+        ft = { "markdown", "Avante", "copilot-chat", "opencode_output" },
+      },
+      -- Optional, for file mentions and commands completion, pick only one
+      "saghen/blink.cmp",
+      -- 'hrsh7th/nvim-cmp',
+
+      -- Optional, for file mentions picker, pick only one
+      "folke/snacks.nvim",
+      -- 'nvim-telescope/telescope.nvim',
+      -- 'ibhagwan/fzf-lua',
+      -- 'nvim_mini/mini.nvim',
+    },
   },
+  -- {
+  --   "NickvanDyke/opencode.nvim",
+  --   enabled = false,
+  --   dependencies = {
+  --     ---@module 'snacks'
+  --     { "folke/snacks.nvim" },
+  --   },
+  --   config = function()
+  --     local map = LazyVim.safe_keymap_set
+  --
+  --     local select = function()
+  --       require("opencode").select()
+  --     end
+  --
+  --     local add = function()
+  --       require("opencode").prompt("@this")
+  --     end
+  --
+  --     local toggle = function()
+  --       require("opencode").toggle()
+  --     end
+  --
+  --     local ask = function()
+  --       require("opencode").ask("@this: ", { submit = true })
+  --     end
+  --
+  --     ---@type opencode.Opts
+  --     vim.g.opencode_opts = {
+  --       -- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition".
+  --     }
+  --
+  --     -- Required for `opts.events.reload`.
+  --     vim.o.autoread = true
+  --
+  --     map("n", "<leader>ao", "", { desc = "opencode" })
+  --     map("n", "<leader>aot", function()
+  --       toggle()
+  --     end, { desc = "Toggle" })
+  --     map("n", "<leader>aoa", function()
+  --       ask()
+  --     end, { desc = "Ask this" })
+  --     map({ "n", "v" }, "<leader>aos", function()
+  --       select()
+  --     end, { desc = "Execute opencode action…" })
+  --     map({ "n", "x" }, "<leader>ao>", function()
+  --       add()
+  --     end, { desc = "Add to opencode" })
+  --   end,
+  -- },
 }
